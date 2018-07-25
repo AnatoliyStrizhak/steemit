4d4c6b94559cb4260ad0729b84e052658c49a714
Anatoliy Strizhak <jrodger@yandex.ru>
2018-07-25 15:08:46 +0200

    Add Steemit Voting Power plugin

---
+++ voting_power/getpwr.js
@@ -0,0 +1,51 @@
+var bat1="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA4AAAAUCAYAAAC9BQwsAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4gcZDA4IkSuI9AAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAACIElEQVQ4y32TS2uTQRSGn/N9k5CmMZcm0TS1F4UqSCoWXfgHFLGCXQmKdOMvcKN0UQWVgjtR3FTQlQvXLUI1Ii4UN+JCRGyFFHsxNCa2TW3SfMmMG4ttM/EsZ+aZd84zHDFadwPHATCmguN4udnZYLm8BgZ6enuJJhJvMSYA/AYagCdG6xhCBCQBdK78yM/H0p0fQ+EoGPArh2KpeBIoAQqti4hUFUJm5vOXPWOjo3eMoXNjs7IWicVRrgtAQxsunh9+X9ea9kCA+48mLoXD4deSn58/13Ww//GN69eT+5JxWpWIw4tsluyr7MzKenlE5QuFZDIRZ+zWTR4u3+OXXm6CDNDj7+PpyBMC0dghap6ov1IwusFSLcdc5SuC7EI1JiRoZyse1PbttEoSDCrLQw0ptReRLZLNbaeExXqR1fqyjcPnRnYs7bi+gYcnHqbJDHhSB2PsYJdKExC/VU63kwIRC6gNU6vPKdQWsATSExrkQuyyBXQMd/ePU9M16z+2OW3gtehxfPE2C/Ul3F1NusDh4AAPOiZsoMPV9BgbptIsB4i4odaJ13JXyNfmrD0eCJ1gMjXdDBo0z/ongarValCiULVYdXA59e0M+dp3a2Jf+zHepd78A43WcQfJiUPyw5FPaLTVquv68a2brVFBZTKZn6VSgaMDg7jKaT1WCKtrZYbPDoFSiGnoDm+zOjg9/bJDjOZ/5fP7OD00BFpP/QEIVsgybJ3tVAAAAABJRU5ErkJggg==";
+var bat2="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA4AAAAUCAYAAAC9BQwsAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4gcZDAoI9UdN8AAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAABwUlEQVQ4y5WSsWsUURCHv7e7ebd7e3s5iBu5SDTYaWuTaCGnncRGSysRLKONFv4FYieIggQrEdMZEWKjghbBTkM0WnhB4uF5InLR27vo7o7NRl2zD8nA8BiYb+Y380Z9WFsLwjDcqbUWYB9KRbPXbzxqf2yjbItgeJiZ8+f2IzKGUi1AgFUV9XpeyXXH+t1v46nFSOB5b2qj9eUhrQGhNxjQXHk15VcqexzLir0geItIkyiKpu/O3hJlD4nrB6L9qtTru3Lu+kHHLVc6oDpXL19ZkTQ5QNT7fhqQT++akiSJ2eNYHi88lImJvR0RmXRQFgCj47tBKYymFNVajfhnDIDFdi2rvX0wM+fvQIiBH4ZU2wz2OUTKyz96ctgJFBeKwTKLhdCmHpHnACGwkQMjDpOylB3Hvx1PAjPFHT0eAGnhKhUOsGSa8TgJLwrlOkyj1MUcWAU+A6HLTSAyfN8ORNo5cH0z2OASKauGrTaAU8VSS1wD+obNloBWMTjgLCnLhhmPAWe2govPnjLVuA8ixhttt+6htf4Nvl6Yn39y8OiRxn/O8wvA3O07IDJQ692uW/b9wLbtkayQNoCl7P2KyPtfAzmyM6IXkXAAAAAASUVORK5CYII=";
+var bat3="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA4AAAAUCAYAAAC9BQwsAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4gcZDAw6a8q79gAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAABlElEQVQ4y72SMWsUURSFvztvkpnMzA4jUYcgqNlsWLAQJH0gkiqNlaYSLEzhH7DQ/2MW7CzSiLApUgQkdqbQmDqEjbiJWTLZ967FLnGDs0NI4YEDr7iHc+65T9S5CeAOcAMwiBy9X1/f+/HtO4ggwOu3b54gcgAcDef2ZChc7J2cWAedOJg8rmU396eSBIBO9xc77fZS40Gzh3I/TtNj4AuqeuvDu9bWZBjthkmqQZJqns/o7SHzfEbjNNMwSTWqZbr24vm2qj4VtTYSY35//bzD3eY8qko5hH5xxux8k61PH5/5WDsNUG/MEcQxVbBuijAIOAc8IAQqnMrhAdNcAx7XhAec/XfHi7qRqwv9v08F60AEyhoWqYi6vAzGgO//yywD70Kc+JeEGxvjs4kMEg2QXt5xdRWiqJxzDTBeyY7qsK0WWpyPaVFHHUeEIpiVFdjcHFOjjy2KEaE3sO92fhK225UnMLaPdQ5xio8x/Vcv1w7zxuyV7le/V+fhwqOeqHMRIo/19JTC2rj6tygTtbSL6u4f52uMhVtnPTsAAAAASUVORK5CYII=";
+
+
+function get_pwr()
+{
+    steem.api.getAccounts(['astrizak'], function(err, response){
+
+        var res=response[0]['voting_power']/100;
+
+        if(res<60)
+        {
+            document.getElementById('pwr_img').src = bat3;
+        }
+
+        if(res>70)
+        {
+            document.getElementById('pwr_img').src = bat2;
+        }
+
+        if(res>90)
+        {
+            document.getElementById('pwr_img').src = bat1;
+        }
+
+        document.getElementById('pwr_img').title = res+"%";
+
+    });
+}
+
+
+if(location.hostname.match(/steemit.com/i) && document.getElementById("content") && document.getElementsByClassName("small-7 large-4 columns Header__buttons"))
+{
+    var node = document.createElement("div");
+    node.id = "pwr";
+    node.innerHTML = "<img src='./icon.png' id='pwr_img' style='height:20px! important; margin-left:10px;' title=''>";
+    document.getElementsByClassName("small-7 large-4 columns Header__buttons")[0].appendChild(node);
+
+    get_pwr();
+}
+
+document.addEventListener('click', function(e) {
+    var target = e.target
+    currentItem = target.parentElement;
+    if (currentItem.className!=null && currentItem.className == "entry-title") 
+    {
+        get_pwr();
+    }
+}, false);
+
